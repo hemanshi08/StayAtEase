abc21dd112fcef36d6acbde0d4f6173a5a7775c4
Arati Paraliya <paraliyarati.com>
2025-03-15 14:49:37 +0530

    add all propertyreview page

--- StayAtEase_frontend/src/App.jsx
+++ StayAtEase_frontend/src/App.jsx
@@ -1,26 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";
-import PropertyCard from "./Components/Property_card";
-import HomePage from "./rental/home";
-import PropertyListing from "./rental/Propertylist";
-import Wishlist from "./rental/wishlist";
-import PropertyForm from "./Admin/Propertyform";
-import PropertyReviews from "./Admin/propertyreview";
-import Dashboard from "./Admin/dashboard";
-import PropertyListings from "./Admin/propertylisting";
-import PropertyMessages from "./Admin/PropertyMessages";
-import PropertyDetails from "./rental/propertydetails";
-import AdminPropertyDetails from "./Admin/propertydetails";
-import ProfileDetails from "./Admin/profiledetails";
-
+import AllpropertyReviews from "./super Admin/AllPropertyReview";
 function App() {
   return (
     <Router>
      
       {/* <PropertyDetails/> */}
       {/* <AdminPropertyDetails/> */}
-      <ProfileDetails/>
+      {/* <ProfileDetails/> */}
+      <AllpropertyReviews/>
     </Router>
   );
 }
