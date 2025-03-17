c36d44c2df5606b78f9ec6660ddbce71c63acf4d
Arati Paraliya <paraliyarati.com>
2025-03-17 13:14:48 +0530

    commit

--- StayAtEase_frontend/src/App.jsx
+++ StayAtEase_frontend/src/App.jsx
@@ -1,19 +1,10 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import AllpropertyReviews from "./super Admin/AllPropertyReview";
-import AllPropertyMessages from "./super Admin/AllPropertyMassges";
-import UserTable from "./super Admin/TatalUser";
-import AdminData from "./super Admin/TotalAdmin";
-import AdminTable from "./super Admin/TotalAdmin";
-import PropertyTable from "./super Admin/AllProperty";
+import HomePage from "./rental/home";
 function App() {
   return (
     <Router>
      
-      {/* <PropertyDetails/> */}
-      {/* <AdminPropertyDetails/> */}
-      {/* <ProfileDetails/> */}
-      {/* <AllPropertyMessages/> */}
-    <PropertyTable/>
+  <HomePage/>
     </Router>
   );
 }
