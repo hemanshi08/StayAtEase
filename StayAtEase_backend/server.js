aa3dfd164b42e3b3df1b965b2018c91882260da1
HEMANSHI GARNARA <hemanshigarnara2004@gmail.com>
2025-04-03 08:50:46 +0530

    pull code from main branch

---
+++ StayAtEase_backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// PostgreSQL Connection
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Backend is running...");
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
