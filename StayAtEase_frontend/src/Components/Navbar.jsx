17f9d224d31d9ac4de35d9ab75fecbc209c64e1c
HEMANSHI GARNARA <hemanshigarnara2004@gmail.com>
2025-04-06 21:09:29 +0530

    add margin top in tenantLayout file

--- StayAtEase_frontend/src/Components/Navbar.jsx
+++ StayAtEase_frontend/src/Components/Navbar.jsx
@@ -11,8 +11,8 @@ export default function Navbar() {
   const isActive = (path) => location.pathname === path ? "text-blue-500 font-bold" : "text-gray-700";
   return (
     <>
-    {/* <nav className="fixed top-0 left-0 w-full bg-white shadow-md p-4 z-50"> */}
-      <nav className="w-full bg-white shadow-md p-4">
+    <nav className="fixed top-0 left-0 w-full bg-white shadow-md p-4 z-50">
+      {/* <nav className="w-full bg-white shadow-md p-4"> */}
         <div className="container mx-auto flex justify-between items-center">
           {/* Logo */}
           <a href="#" className="flex items-center space-x-2">
